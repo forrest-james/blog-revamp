99654f3d9509b44b18898f789bea47f00a28ada1
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-02-04 15:10:29 -0600

    Added EF Database Migration

--- source/Domain/Persistence/ApplicationDbContext.cs
+++ source/Domain/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Objects;
 using Domain.Persistence.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,5 +13,29 @@ namespace Domain.Persistence
             : base(options) { }
 
         public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Post>()
+                .HasMany(post => post.Comments)
+                .WithOne(comment => comment.Parent);
+            builder.Entity<Post>()
+                .HasMany(post => post.Reactions)
+                .WithOne();
+            builder.Entity<Reaction>()
+                .Property(reaction => reaction.Parent)
+                .HasConversion(
+                    parent => parent.Id,
+                    id => Posts.Find(id)
+                );
+            builder.Entity<Reaction>()
+                .HasKey(reaction => new
+                {
+                    reaction.Parent,
+                    reaction.IpAddress
+                });
+
+        }
     }
 }
