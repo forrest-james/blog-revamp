6e207442e50f0eb2b1ba4ffb70c0a53b522227a0
James Forrest <61599518+forrest-james@users.noreply.github.com>
2021-02-04 14:20:53 -0600

    Update Startup.cs

--- source/UI/Startup.cs
+++ source/UI/Startup.cs
@@ -1,5 +1,9 @@
+using Domain.Persistence;
+using Domain.Persistence.Identity;
+using Domain.Persistence.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,6 +21,17 @@ namespace UI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string _connectionString = string.Format("{0};{1};{2};{3};",
+                _configuration["SqlConnection:Server"],
+                _configuration["SqlConnection:Database"],
+                _configuration["SqlConnection:Security"],
+                _configuration["SqlConnection:Options"]);
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(_connectionString));
+            services.AddAuthentication();
+            services.AddAuthorization();
+            services.AddIdentity<BlogUser, BlogRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();            
             services.ConfigureApplicationCookie(options =>
             {
                 options.LoginPath = "/login";
@@ -28,6 +43,8 @@ namespace UI
                 options.Cookie.HttpOnly = true;
                 options.Cookie.IsEssential = true;
             });
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddScoped<PostRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
